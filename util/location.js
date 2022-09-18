d87a8259ab3ffd91b163d9c3d09e2725043cc75d
luongbodoi <88548776+luongbodoi@users.noreply.github.com>
2022-09-18 23:35:26 +0700

    update location.js

--- util/location.js
+++ util/location.js
@@ -9,19 +9,6 @@ async function getCoordsForAddress(address) {
         lat: 40.213231,
         lng: 34.23123123,
     };
-
-    // const response = await axios.get(`API key`);
-
-    // const data = response.data;
-
-    // if (!data || data.status === 'ZERO_RESULTS') {
-    //     const error = new HttpError('Could not find location', 422);
-    //     throw error;
-    // }
-
-    // const coordinates = data.results[0].geometry.location;
-
-    // return coordinates;
 }
 
 module.exports = getCoordsForAddress;
